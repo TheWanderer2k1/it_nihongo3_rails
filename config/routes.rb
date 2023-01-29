8bb852fd3ff60f02d6a3f99729427e7dd9adfd96
hoanganh <hoanganhlhbk@gmail.com>
2023-01-29 14:42:09 +0700

    add profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get '/', to: "static_pages#home"
+  get 'users/show'
+  root 'static_pages#home'
   devise_for :users
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
